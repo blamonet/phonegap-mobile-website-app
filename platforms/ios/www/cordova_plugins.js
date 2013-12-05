9e3614b246a78f07e044ab94577bdf47f8cd10b4
Aaron Stuart Grant <blamonet@gmail.com>
2013-12-04 20:35:01 -0500

    Adding networking connection check

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -6,6 +6,21 @@ module.exports = [
         "clobbers": [
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
     }
 ]
 });
--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -22,10 +22,32 @@ var app = {
         this.bindEvents();
         document.addEventListener("deviceready", onDeviceReady, false);
         function onDeviceReady() {
-            var ref = window.open(encodeURI('http://www.toddler-cards.blamo.org'), '_self', 'location=no');
+            console.log('Application Ready');
+            if (checkConnection()) {
+              var ref = window.open(encodeURI('http://www.toddler-cards.blamo.org'), '_self', 'location=no');
+            } else {
+              var ref = window.open('offline.html', '_self', 'location=no');
+            }
         }
+        function checkConnection() {
+            var networkState = navigator.connection.type;
 
+            var states = {};
+            states[Connection.UNKNOWN]  = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI]     = 'WiFi connection';
+            states[Connection.CELL_2G]  = 'Cell 2G connection';
+            states[Connection.CELL_3G]  = 'Cell 3G connection';
+            states[Connection.CELL_4G]  = 'Cell 4G connection';
+            states[Connection.CELL]     = 'Cell generic connection';
+            states[Connection.NONE]     = 'No network connection';
 
+            if (states[networkState] == 'No network connection') {
+              return false;
+            } else {
+              return true;
+            }
+        }
     },
     // Bind Event Listeners
     //
